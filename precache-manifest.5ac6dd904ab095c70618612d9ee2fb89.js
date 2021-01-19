3a99e1f7b16aa17e5308c3ef13b8ad824a09cdda
Vokhmintseva <lycaste@mail.ru>
2021-01-19 13:41:36 +0300

    Updates

---
+++ precache-manifest.5ac6dd904ab095c70618612d9ee2fb89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55de3b8a975af5a810e0df1dcddacf1d",
+    "url": "/photo_editor/index.html"
+  },
+  {
+    "revision": "41d91a2038f59aae7590",
+    "url": "/photo_editor/static/css/main.153399a1.chunk.css"
+  },
+  {
+    "revision": "a86587ad981f5531540b",
+    "url": "/photo_editor/static/js/2.d0be4b04.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/photo_editor/static/js/2.d0be4b04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d91a2038f59aae7590",
+    "url": "/photo_editor/static/js/main.f8a9eb5f.chunk.js"
+  },
+  {
+    "revision": "7a73e3151ddf617f7c3e",
+    "url": "/photo_editor/static/js/runtime-main.c9fd351c.js"
+  }
+]);
